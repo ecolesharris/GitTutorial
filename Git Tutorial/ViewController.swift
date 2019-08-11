06137904c8a8be9afc06917e2ccaf4ed10478a5c
Evan Coles-Harris <ecolesharris@gmail.com>
2019-08-11 14:31:16 -0600

    Changed the message from "Hello Git!" to "This is a message!"

---
+++ Git Tutorial/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Git Tutorial
+//
+//  Created by Evan Coles-Harris on 4/20/19.
+//  Copyright © 2019 Evan's Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let message = "This is a message!"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(message)
+    }
+
+
+}
+
